0c8ef1d875392887a85349136c70d324694a66ec
notfust <romish.monster@gmail.com>
2023-10-07 11:36:30 +0300

    first commit

---
+++ fsm.c
@@ -0,0 +1,8 @@
+//
+//  fsm.c
+//  FSM
+//
+//  Created by test on 07.10.2023.
+//
+
+#include "fsm.h"
---
+++ fsm.h
@@ -0,0 +1,13 @@
+//
+//  fsm.h
+//  FSM
+//
+//  Created by test on 07.10.2023.
+//
+
+#ifndef fsm_h
+#define fsm_h
+
+#include <stdio.h>
+
+#endif /* fsm_h */
